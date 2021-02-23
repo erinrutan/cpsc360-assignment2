f15810618614c0e3a4e888c964cca9bf981e033e
erinrutan <47155978+erinrutan@users.noreply.github.com>
2021-02-23 11:05:04 -0500

    Add files via upload

---
+++ solution.h
@@ -0,0 +1,17 @@
+/*
+ * solution.h
+ *
+ *  Created on: Feb 17, 2021
+ *      Author: erin
+ */
+
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+
+class solution {
+public:
+	solution();
+	virtual ~solution();
+};
+
+#endif /* SOLUTION_H_ */
