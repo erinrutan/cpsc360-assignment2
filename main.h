f5bd45ef1320ee64b845f813c9b11cf4d43d2004
erinrutan <47155978+erinrutan@users.noreply.github.com>
2021-02-23 11:10:35 -0500

    Add files via upload

---
+++ main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Feb 17, 2021
+ *      Author: erin
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class main {
+public:
+	main();
+	virtual ~main();
+};
+
+#endif /* MAIN_H_ */
